eb5eddfa2a49ae05180100d16a77f36d9ea35911
javaeecub@gmail.com <javaeecub@gmail.com>
2015-08-19 20:25:05 +0530

    code changes

--- src/main/java/com/hibernatemanymanybidirectional/app/App.java
+++ src/main/java/com/hibernatemanymanybidirectional/app/App.java
@@ -13,7 +13,7 @@ public class App {
 	public static void main(String[] args) {
 		try (ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring-beans.xml")) {
 			BooksAuthorsService booksAuthorsService = (BooksAuthorsService) ctx.getBean("booksAuthorsServiceImpl");
-			//booksAuthorsService.booksAuthorsPersistenceOperations();
+			booksAuthorsService.booksAuthorsPersistenceOperations();
 			System.out.println(booksAuthorsService.getDatefromSysDate());
 			System.out.println(booksAuthorsService.getGregorianCalendarfromSysDate().getTime());
 		}
@@ -24,6 +24,14 @@ public class App {
 
 /**
  * OUTPUT
+ * 
+ * Author [authorId=67, authorName=Rod johnson]
+ * Author [authorId=69, authorName=Rod johnson]
+ * Author [authorId=71, authorName=Rod johnson]
+ * Author [authorId=73, authorName=Rod johnson]
+ * Author [authorId=75, authorName=Rod johnson]
+ * Author [authorId=77, authorName=Rod johnson]
+ * Author [authorId=79, authorName=Rod johnson]
  * 
  * 2015-08-19
  * Wed Aug 19 00:00:00 IST 2015
